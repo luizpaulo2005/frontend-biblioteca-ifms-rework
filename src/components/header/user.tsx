3de5266177e39801e89b12562c3c34703f574d03
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-02-27 22:03:31 -0300

    link home header

--- src/components/header/user.tsx
+++ src/components/header/user.tsx
@@ -1,7 +1,8 @@
 export function HeaderUser(){
     return (
-        <div className="w-full h-24 rounded-md bg-green-600 flex justify-between text-white p-2 dark:bg-green-800">
-            <div className="self-center font-semibold text-4xl ml-1">Biblioteca Digital - IFMS</div>
+
+            <div className="w-full h-24 rounded-md bg-green-600 flex justify-between text-white p-2 dark:bg-green-800">
+                    <a href="/" className="self-center font-semibold text-4xl ml-1">Biblioteca Digital - IFMS</a>
             <div className="self-center overflow-hidden">
                 <button className="bg-slate-600 rounded-md pt-2 pb-2 pl-4 pr-4 m-1 transition-colors hover:bg-slate-500">Botão</button>
                 <button className="bg-slate-600 rounded-md pt-2 pb-2 pl-4 pr-4 m-1 transition-colors hover:bg-slate-500">Botão</button>
@@ -9,5 +10,6 @@ export function HeaderUser(){
                 <button className="bg-blue-600 rounded-md pt-2 pb-2 pl-4 pr-4 m-1 transition-colors hover:bg-slate-500">Login</button>
             </div>
         </div>
+
     )
 }
